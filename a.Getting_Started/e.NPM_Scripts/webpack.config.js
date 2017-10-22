9869fc2da5fdb13ea01674ad7e8bb4d680ee8f48
kienshin <1034131477>
2017-10-22 15:19:16 +0800

    getting started/e.NPM_Scripts

---
+++ a.Getting_Started/e.NPM_Scripts/webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kienshin on 17/10/22.
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
