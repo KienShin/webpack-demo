005e425e681a8eec8aa66516c5380a398c0b8449
kienshin <1034131477>
2017-10-22 15:33:23 +0800

    b.Asset_Management/b.Loading_CSS

---
+++ b.Asset_Management/b.Loading_CSS/src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kienshin on 17/10/22.
+ */
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ b.Asset_Management/b.Loading_CSS/webpack.config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by kienshin on 17/10/22.
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
