772a184bb5e18d31c8ad3739cbbb8b38ae089a03
kienshin <1034131477>
2017-10-22 15:43:34 +0800

    b.Asset_Management/c.Loading_Images

---
+++ b.Asset_Management/c.Loading_Images/webpack.config.js
@@ -0,0 +1,29 @@
+/**
+ * Created by kienshin on 17/10/22.
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
