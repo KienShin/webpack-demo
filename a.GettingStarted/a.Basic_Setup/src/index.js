782cc272d163fcc6145c4cb40364d92822d73881
kienshin <1034131477>
2017-10-22 15:09:57 +0800

    getting started/b.Creating_a_Bundle

--- a.GettingStarted/a.Basic_Setup/src/index.js
+++ a.GettingStarted/a.Basic_Setup/src/index.js
@@ -4,7 +4,7 @@
 function component() {
   var element = document.createElement('div');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
